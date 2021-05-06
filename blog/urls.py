0ac0d4b8a8b55cc08941f28e9dbd9a9318fc3a14
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2021-05-06 19:45:41 +0200

    adding articles app with url and views and html

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
     path('', views.home),
+    path('articles/', include('articles.urls'))
 ]
