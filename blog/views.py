fb0e29cf71127990ef1155b1f2fedf88d6ad2f62
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2021-05-06 19:10:26 +0200

    render html files

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("home page")
+    # return HttpResponse("home page")
+    return render(request, 'home.html')
 
 
 def about(request):
-    return HttpResponse("about page")
+    # return HttpResponse("about page")
+    return render(request, 'about.html')
