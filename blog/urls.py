f286fdd008b3789ea4b62dd476298622fd906d29
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2021-05-07 17:55:34 +0200

    added thumb to article

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -9,3 +11,5 @@ urlpatterns = [
     path('', views.home),
     path('articles/', include('articles.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
