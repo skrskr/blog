0c8792fa36e509b847ad66625d02b43ff3d269c9
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2021-05-07 17:04:11 +0200

    added url paramter as slug

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('', views.article_list),
+    url(r'^(?P<slug>[\w-]+)$', views.article_detail)
 ]
