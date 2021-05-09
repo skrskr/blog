e80205fccd3cae38a9d92ec5564b9c3e9918834b
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2021-05-09 13:36:17 +0200

    added create blog form

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from .models import Article
+from . import forms
 
 def article_list(request):
     articles = Article.objects.all().order_by("date")
@@ -15,5 +16,13 @@ def article_detail(request, slug):
     
 @login_required(login_url="/accounts/login")    
 def article_create(request):
-    return render(request, 'articles/article_create.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            # save article to db
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+
+    return render(request, 'articles/article_create.html', {'form': form})
     
