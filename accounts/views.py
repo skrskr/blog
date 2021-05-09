4d0ac1aceacfe58c9c26eb0f3b6b92f8450e94cd
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2021-05-09 12:55:15 +0200

    login is requrired for create blog

--- accounts/views.py
+++ accounts/views.py
@@ -21,6 +21,8 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get("next"))
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from .models import Article
 
@@ -11,4 +12,8 @@ def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+    
+@login_required(login_url="/accounts/login")    
+def article_create(request):
+    return render(request, 'articles/article_create.html')
     
